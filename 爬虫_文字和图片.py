28f7b62b9e59325f5086830acc40c6f0d2a2f4ee
wangyifan349 <86041125+wangyifan349@users.noreply.github.com>
2023-03-15 12:39:16 +0000

    Create 爬虫_文字和图片.py

---
+++ 爬虫_文字和图片.py
@@ -0,0 +1,45 @@
+import os
+import requests
+from bs4 import BeautifulSoup
+from fake_useragent import UserAgent
+
+# 获取谷歌浏览器User-Agent
+def get_google_chrome_user_agent():
+    ua = UserAgent()
+    return ua.google
+
+# 爬取图片并保存到本地
+def save_image(url, save_path):
+    response = requests.get(url, stream=True)
+    with open(save_path, 'wb') as f:# 以二进制写入的方式打开文件
+        for chunk in response.iter_content(chunk_size=8192):#按块读取数据
+            if chunk:#如果存在数据
+                f.write(chunk)#写入数据
+
+# 爬取网页中的图片和文字
+def crawl_images_and_text(url, img_folder, text_file):
+    headers = {'User-Agent': get_google_chrome_user_agent()} # 使用谷歌浏览器User-Agent
+    response = requests.get(url, headers=headers)
+    soup = BeautifulSoup(response.content, 'html.parser')
+
+    # 保存图片
+    images = soup.find_all('img')
+    if not os.path.exists(img_folder):
+        os.makedirs(img_folder)
+
+    for img in images:
+        img_url = img.get('src')#获取图片的 URL 链接
+        img_name = img_url.split('/')[-1]#提取图片名字
+        save_path = os.path.join(img_folder, img_name)# 拼接保存路径
+        save_image(img_url, save_path)# 保存图片到本地
+
+    # 提取并保存文字
+    with open(text_file, 'w') as f:
+        for text in soup.stripped_strings:#遍历所有的字符串，并去除前后空格
+            f.write(text + '\n')
+
+if __name__ == '__main__':
+    url = input("请输入要爬取的网址：")
+    img_folder = input("请输入保存图片的文件夹：")
+    text_file = input("请输入保存文字的文件名：")
+    crawl_images_and_text(url, img_folder, text_file)
